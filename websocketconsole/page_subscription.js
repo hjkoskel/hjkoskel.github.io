0e3f126b6354d55d93a9efb8062f7ced37e9eb3b
hjkoskel <hjkoskel@gmail.com>
2019-05-07 23:27:10 +0300

    added gz json support

--- websocketconsole/page_subscription.js
+++ websocketconsole/page_subscription.js
@@ -1,5 +1,6 @@
 var PageSubscription=`
 <div>
+
   <a class="button is-success" on-click="['downloadAllInConnectionTopic',selectedConnection,selectedTopic]"  >
     <span class="icon is-small">
       <i class="fas fa-check"></i>
@@ -25,48 +26,36 @@ var PageSubscription=`
   <br/>
 
   {{#recieved[selectedConnection][selectedTopic]:msgIndex}}
-      <span class="tag is-warning">{{format}}</span>
-      {{#if format!="ipfs"}}
-        <span class="tag is-warning">{{size}} bytes</span>
-      {{/if}}
+    <span class="tag is-warning">{{format}}</span>
+    {{#if format!="ipfs"}}
+      <span class="tag is-warning">{{size}} bytes</span>
+    {{/if}}
 
-      <article class="message {{formatByMessageFormat(format)}}">
-
+    <article class="message {{formatByMessageFormat(format)}}">
       <div class="message-header">
         <p>{{formatDateTimeDoNotRepeatDate(time)}}  {{topic}}</p>
-
         <span>
-        <a class="button is-link" on-click="['checkMIME',selectedConnection,selectedTopic,msgIndex]"  >
-          <span>Test MIME</span>
-        </a>
-
-
-        {{#if format=="ipfs"}}
-          {{#if !loadedIPFS}}
-            <a class="button is-link" on-click="['downloadIPFS',selectedConnection,selectedTopic,msgIndex,ipfsHash]"  >
-              <span>IPFS get</span>
-            </a>
+          <a class="button is-link" on-click="['checkMIME',selectedConnection,selectedTopic,msgIndex]"  >
+            <span>Test MIME</span>
+          </a>
+          {{#if format=="ipfs"}}
+            {{#if !loadedIPFS}}
+              <a class="button is-link" on-click="['downloadIPFS',selectedConnection,selectedTopic,msgIndex,ipfsHash]"> <span>IPFS get</span> </a>
+            {{/if}}
           {{/if}}
-        {{/if}}
-        <a class="button is-success" on-click="['downloadSingle',selectedConnection,selectedTopic,msgIndex]"  >
-          {{#if saved}}
-          <span class="icon is-small">
-            <i class="fas fa-check"></i>
-          </span>
-          {{/if}}
-          <span>Download</span>
-        </a>
-
-        <a class="button is-danger is-outlined" on-click="['deleteSingle',selectedConnection,selectedTopic,msgIndex]"  >
-          <span>Delete</span>
-          <span class="icon is-small">
-            <i class="fas fa-times"></i>
-          </span>
-        </a>
+          <a class="button is-success" on-click="['downloadSingle',selectedConnection,selectedTopic,msgIndex]"  >
+            {{#if saved}}
+              <span class="icon is-small"> <i class="fas fa-check"></i> </span>
+            {{/if}}
+            <span>Download</span>
+          </a>
+          <a class="button is-danger is-outlined" on-click="['deleteSingle',selectedConnection,selectedTopic,msgIndex]"  >
+            <span>Delete</span>
+            <span class="icon is-small"> <i class="fas fa-times"></i> </span>
+          </a>
         </span>
       </div>
       <div class="message-body">
-
         {{#if format=="binary"}}
           <img src={{formatPicIfPossible(payload)}}/>
         {{else}}
